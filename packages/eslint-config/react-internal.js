7e0a940529e27e4904bc9bafc194ac228286b672
Lucas <50617944+lucasporrini@users.noreply.github.com>
2025-06-17 02:01:52 +0200

    fix: pipeline fixes

--- packages/eslint-config/react-internal.js
+++ packages/eslint-config/react-internal.js
@@ -1,17 +1,17 @@
-import js from "@eslint/js"
-import eslintConfigPrettier from "eslint-config-prettier"
-import pluginReact from "eslint-plugin-react"
-import pluginReactHooks from "eslint-plugin-react-hooks"
-import globals from "globals"
-import tseslint from "typescript-eslint"
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
-import { config as baseConfig } from "./base.js"
+import { config as baseConfig } from "./base.js";
 
 /**
  * A custom ESLint configuration for libraries that use React.
  *
  * @type {import("eslint").Linter.Config} */
-export const config = [
+module.exports = [
   ...baseConfig,
   js.configs.recommended,
   eslintConfigPrettier,
@@ -38,4 +38,4 @@ export const config = [
       "react/prop-types": "off",
     },
   },
-]
+];
