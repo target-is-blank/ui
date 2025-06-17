ef9bc98bd5628666a9e0ede720a5838f9100a750
Lucas <50617944+lucasporrini@users.noreply.github.com>
2025-06-17 02:08:22 +0200

    fix: pipeline fixes

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -5,7 +5,7 @@ import pluginReact from "eslint-plugin-react";
 import pluginReactHooks from "eslint-plugin-react-hooks";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import { config as baseConfig } from "./base";
+import { config as baseConfig } from "./base.js";
 
 /**
  * A custom ESLint configuration for libraries that use Next.js.
