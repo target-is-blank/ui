02029109e185e8ebe6928c4c4ffe2ea6eaf26d6e
Lucas <50617944+lucasporrini@users.noreply.github.com>
2025-07-17 17:27:54 +0200

    feat: number mosaic

---
+++ apps/www/registry/demo/components/number-mosaic/index.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { NumberMosaic } from "@/registry/components/number-mosaic";
+
+export const NumberMosaicDemo = () => {
+  return <NumberMosaic value={1234567890} random gap={3} />;
+};
