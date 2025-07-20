606526efc119576ed10124ff0dc5da8a6581c2d1
Lucas <50617944+lucasporrini@users.noreply.github.com>
2025-07-20 02:47:09 +0200

    fix: number mosaic responsive

--- apps/www/registry/demo/components/number-mosaic/index.tsx
+++ apps/www/registry/demo/components/number-mosaic/index.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { NumberMosaic } from "@/registry/components/number-mosaic";
+import { useIsMobile } from "@workspace/ui/hooks/use-mobile";
 
 export const NumberMosaicDemo = () => {
-  return <NumberMosaic value={1234567890} random gap={3} />;
+  const isMobile = useIsMobile();
+  return <NumberMosaic value={isMobile ? 1234 : 123456} random gap={3} />;
 };
